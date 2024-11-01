558cb9381e06fb4d862adb6639004c5de27464f2
H4ppier <127703764+H4ppier@users.noreply.github.com>
2024-11-01 11:49:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import Navbar from './Navbar';
-import LandingPage from './LandingPage';
-import Prediction from './Prediction';
-import reportWebVitals from './reportWebVitals';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+// Import MUI's ThemeProvider and createTheme for applying a theme
+import CssBaseline from '@mui/material/CssBaseline'; 
+// CssBaseline for consistent baseline styles across browsers
+import { BrowserRouter as Router } from 'react-router-dom'; // Ensure this is added
+import App from './App';
 
+const theme = createTheme();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Navbar />
-    <Prediction />
-  </React.StrictMode>
+<React.StrictMode>
+{/* Wrap the app in ThemeProvider and pass the theme for consistent styling*/}
+<ThemeProvider theme={theme}>
+<CssBaseline />
+<Router>
+<App />
+</Router>
+</ThemeProvider>
+</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
